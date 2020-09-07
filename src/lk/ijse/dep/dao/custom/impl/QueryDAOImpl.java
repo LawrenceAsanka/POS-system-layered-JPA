8e74eb5cf0def4d0741356f661e15e4c57ac1c88
LawrenceAsanka <asankalorance@gmail.com>
2020-09-07 07:47:41 +0530

    Add test directory to .gitignore

--- src/lk/ijse/dep/dao/custom/impl/QueryDAOImpl.java
+++ src/lk/ijse/dep/dao/custom/impl/QueryDAOImpl.java
@@ -7,6 +7,9 @@ import lk.ijse.dep.dao.custom.QueryDAO;
 import lk.ijse.dep.entity.CustomEntity;
 import org.hibernate.transform.Transformers;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 public class QueryDAOImpl implements QueryDAO {
@@ -20,9 +23,14 @@ public class QueryDAOImpl implements QueryDAO {
 
     @Override
     public List<CustomEntity> getOrderDetail() throws Exception {
-        return entityManager.createNativeQuery("SELECT o.id AS orderId,o.date AS orderDate,c.id AS customerId,c.name AS customerName,(SUM(od.qty*od.unitPrice)) AS total FROM `Order` o\n" +
+        List<CustomEntity> orderDetail = new ArrayList<>();
+        List<Object[]> resultList = entityManager.createNativeQuery("SELECT o.id AS orderId,o.date AS orderDate,c.id AS customerId,c.name AS customerName,SUM(od.qty*od.unitPrice) AS total FROM `Order` o\n" +
                 "INNER JOIN OrderDetail od on o.id = od.orderId\n" +
                 "INNER JOIN Customer c on o.customerId = c.id\n" +
                 "GROUP BY o.id").getResultList();
+        for (Object[] result :resultList ) {
+            orderDetail.add(new CustomEntity((String) result[0],(Date) result[1],(String)result[2],(String) result[3],(BigDecimal) result[4]));
+        }
+        return orderDetail;
     }
 }
